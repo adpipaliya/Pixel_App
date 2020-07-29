782ac010cff09dec7698d070bceaaa9a1658afb1
adpipaliya <apipaliya858@rku.ac.in>
2020-07-29 16:44:47 +0530

    Login Page Added Signup page Added Logo Added

--- app/src/main/java/com/example/pixelapp/MainActivity.java
+++ app/src/main/java/com/example/pixelapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.pixelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginbtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, Signup.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
